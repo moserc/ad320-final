f0bda956561e00d2f003b3cc941aca422e299447
pepespasic <pepespasic@gmail.com>
2023-12-03 22:06:03 -0800

    Changed tables.sql file

--- tables.sql
+++ tables.sql
@@ -3,3 +3,33 @@
     Date:
     AD320 Final Project
 */
+
+INSERT INTO items 
+(price,  brand_name, photo_url, rating_review)
+VALUES
+(179.95, "Atlas", "Atlas.PNG", "5.0"),
+(136.73, "Komperdell", "Komperdell.PNG", "1.0"),
+(69.95, "Tubbs", "Tubs.PNG", "4.0"),
+(70.93, "Fischer", "Fischer.PNG", "5.0"),
+(449.95, "Rossignol", "Rossignol.PNG", "5.0"),
+(159.00, "Sonic", "Sonic.PNG", "4.5"),
+(279.29, "Co-op_Cycles", "Co-op_Cycles.PNG", "4.5"),
+(999.00, "Salsa", "SalsaA.PNG", "4.2"),
+(999.00, "Salsa", "SalsaB.PNG", "4.0"),
+(2995.00, "Schwinn", "Schwinn.PNG", "4.5"),
+(2499.99, "Salsa", "SalsaC.PNG", "4.5"),
+(337.39, "Big Agnes", "BigAgnes.PNG", "4.1"),
+(185.00, "TheNorthFace", "TheNorthFace.PNG", "4.5"),
+(179.73, "Klymit", "Klymit.PNG", "5.0"),
+(188.89, "Eureka", "Eureka.PNG", "4.0"),
+(117.73, "Alpine Mountain Gear", "AlpineMountainGear.PNG", "4.0"),
+(76.95, "BearVault", "BearVaultA.PNG", "4.2"),
+(83.95, "BearVault", "BearVaultB.PNG", "4.0"),
+(89.95, "BearVault", "BearVaultC.PNG", "4.5"),
+(94.95, "BearVault", "BearVaultD.PNG", "4.0"),
+(65.00, "Osprey", "Osprey.PNG", "4.6"),
+(83.39, "Co-op Traverse", "Co-opTraverse.PNG", "4.5"),
+(239.95, "Granite Gear", "GraniteGear.PNG", "4.5"),
+(999.95, "Old Town", "OldTown.PNG", "4.0"),
+(1299.00, "EddyLine", "EddyLine.PNG", "4.5"),
+(599.95, "TAHE", "TAHE.PNG", "4.0");
